0104c25a9798fb0395ad10dbbdf704eed94c375a
Thibaut Cornolti <thibaut@cornolti.eu>
2018-06-12 14:37:18 +0200

    [+] Socket interface; AI hash

--- ia/main.py
+++ ia/main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3.5
 
 import argparse
-
-# USAGE: ./zappy_ai -p port -n name -h machine
-# port is the port number
-# name is the name of the team
-# machine is the name of the machine; localhost by default
+from classes import AI, Client
 
 def parse_arg():
-    parser = argparse.ArgumentParser(usage="USAGE: ./zappy_ai -p port -n name -h machine", add_help=False)
+    parser = argparse.ArgumentParser(
+        usage="USAGE: ./zappy_ai -p port -n name -h machine", add_help=False)
     parser.add_argument("-help", action="store_true")
-    parser.add_argument("-p", help="is the port number", required=True)
+    parser.add_argument("-p", help="is the port number", required=True, type=int)
     parser.add_argument("-n", help="is the name of the team", required=True)
-    parser.add_argument("-h", help="is the name of the machine; localhost by default", default="localhost")
+    parser.add_argument("-h",
+                        help="is the name of the machine; localhost by default",
+                        default="localhost")
 
     args = parser.parse_args()
     if args.help:
@@ -25,6 +24,14 @@ def parse_arg():
 
 def main():
     args = parse_arg()
+    with Client(args.h, args.p) as (client, error):
+        if error:
+            exit(error)
+        ai = AI(client)
+        print(ai.ulapid)
+        while client.read_line():
+            ai.next_lap()
+            print(ai.ulapid)
 
 
 if __name__ == "__main__":
