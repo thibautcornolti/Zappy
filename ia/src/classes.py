220f1bb344e4735f882b1b2ebc79b9e6ff9adde1
Thibaut Cornolti <thibaut@cornolti.eu>
2018-06-12 17:34:28 +0200

    [~] Change ia file's architecture

--- ia/src/classes.py
+++ ia/src/classes.py
@@ -1,18 +1,28 @@
 import socket
 import hashlib
 
+
 class AI:
-    def __init__(self, client):
+    def __init__(self, client, team):
         self.client = client
+        self.team = team
         init_hash = b''
         self.uuid = hashlib.sha1(init_hash).hexdigest()[:12]
         self.ulapid = self.uuid
+        if self.client.read_line() != 'WELCOME':
+            raise Exception(
+                "Zappy client have to be connected to a Zappy server!")
+        self.client.write(team)
+        r = self.client.read_line()
+        if r == 'ko':
+            raise Exception("Bad team name")
 
     def next_lap(self):
         self.ulapid = hashlib.sha256(str.encode(self.ulapid)).hexdigest()[:12]
 
-    def run(self):
-        pass
+    def broadcast(self, msg):
+        self.client.write("Broadcast %s %s" % (self.ulapid, msg))
+        return True if self.client.read_line() == 'ok' else False
 
 
 class Client:
@@ -40,4 +50,5 @@ class Client:
         while c != b'\n':
             res += c
             c = self.sock.recv(1)
+        print("Receive: %s" % bytes.decode(res))
         return bytes.decode(res)
--- ia/src/main.py
+++ ia/src/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3.5
 
 import argparse
-from classes import AI, Client
+from src.classes import AI, Client
+
 
 def parse_arg():
     parser = argparse.ArgumentParser(
         usage="USAGE: ./zappy_ai -p port -n name -h machine", add_help=False)
     parser.add_argument("-help", action="store_true")
-    parser.add_argument("-p", help="is the port number", required=True, type=int)
+    parser.add_argument("-p", help="is the port number", required=True,
+                        type=int)
     parser.add_argument("-n", help="is the name of the team", required=True)
     parser.add_argument("-h",
                         help="is the name of the machine; localhost by default",
@@ -22,16 +24,22 @@ def parse_arg():
     return args
 
 
-def main():
-    args = parse_arg()
+def run(args):
     with Client(args.h, args.p) as (client, error):
         if error:
             exit(error)
-        ai = AI(client)
+        ai = AI(client, args.n)
         print(ai.ulapid)
-        while client.read_line():
-            ai.next_lap()
-            print(ai.ulapid)
+        print(ai.broadcast("test"))
+        print(client.read_line())
+
+
+def main():
+    args = parse_arg()
+    try:
+        run(args)
+    except Exception as e:
+        exit("Fatal Error: %s" % e)
 
 
 if __name__ == "__main__":
