4a4b1da8fca7ff1e467ccae53d2ff438df14cdbf
Bastien <bastien.rinck@epitech.eu>
2018-06-12 15:47:11 +0200

    [+] params struct

--- server/include/server.h
+++ server/include/server.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <stddef.h>
+
 #define RBUFFER_SIZE 4096
 #define CMD_SIZE 2048
 
@@ -24,3 +26,12 @@ typedef struct cmd_s {
 	char **param;
 	int nparam;
 } cmd_t;
+
+typedef struct params_s {
+	size_t port;
+	size_t width;
+	size_t height;
+	char **teams;
+	size_t nclt;
+	size_t tickrate;
+} params_t;
