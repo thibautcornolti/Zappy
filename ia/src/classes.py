68901f119b2abc3f425ceaf5f332f1cd71a9890e
Arzad <cedric.thomas@epitech.eu>
2018-06-12 18:36:02 +0200

    [~] Prepare IA branch splitting

--- ia/src/classes.py
+++ ia/src/classes.py
@@ -1,31 +1,44 @@
+# coding = utf-8
+import os
 import socket
 import hashlib
+import time
 
 
-class AIController:
+class HashManager(object):
+
+    @staticmethod
+    def hash(value):
+        return hashlib.sha256(str.encode(value)).hexdigest()[:12]
+
+    @staticmethod
+    def generate():
+        return HashManager.hash(str(time.time() * os.getpid()))
+
+
+class AIController(object):
+
     def __init__(self, client, team):
         self.client = client
         self.team = team
-        init_hash = b''
-        self.uuid = hashlib.sha1(init_hash).hexdigest()[:12]
+        self.uuid = HashManager.generate()
         self.ulapid = self.uuid
         if self.client.read_line() != 'WELCOME':
-            raise Exception(
-                "Zappy client have to be connected to a Zappy server!")
+            raise Exception("Zappy client have to be connected to a Zappy server!")
         self.client.write(team)
         r = self.client.read_line()
         if r == 'ko':
             raise Exception("Bad team name")
 
     def next_lap(self):
-        self.ulapid = hashlib.sha256(str.encode(self.ulapid)).hexdigest()[:12]
+        self.ulapid = HashManager.hash(self.ulapid)
 
     def broadcast(self, msg):
         self.client.write("Broadcast %s %s" % (self.ulapid, msg))
         return True if self.client.read_line() == 'ok' else False
 
 
-class Client:
+class Client(object):
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
