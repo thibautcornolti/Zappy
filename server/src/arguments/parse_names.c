38130ee4733e905a8ce4a0d01ba4370cf47f9680
Hirevo <nipolom@live.fr>
2018-06-13 16:19:31 +0200

    Added argument parsing

---
+++ server/src/arguments/parse_names.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2018
+** zappy
+** File description:
+** parse_names
+*/
+
+#include "server.h"
+
+bool parse_names(size_t ac, const char **av, params_t *params, size_t *i)
+{
+	size_t s = *i;
+
+	params->nteam = 0;
+	while (1) {
+		s += 1;
+		if (s >= ac)
+			break;
+		params->nteam += 1;
+	}
+	if (params->nteam == 0) {
+		dprintf(2, "-n: No teams have been declared.\n");
+		return (false);
+	}
+	CHECK(params->teams = calloc(params->nteam, sizeof(char *)), == 0,
+		false);
+	for (size_t s = 0; s < params->nteam; ++s)
+		CHECK(params->teams[s] = strdup(av[*i + s]), == 0, false);
+	*i += s;
+	return (true);
+}
