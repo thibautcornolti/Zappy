737cf1425926c3d601034b9d1709ba120e597efb
Bastien <bastien.rinck@epitech.eu>
2018-06-19 11:48:25 +0200

    [~] typo

--- server/src/admin/adm_lvlup.c
+++ server/src/admin/adm_lvlup.c
@@ -16,7 +16,7 @@ void lvlup_player(size_t id)
 void adm_lvlup(control_t *ctrl, client_t *cl)
 {
 	(void)ctrl;
-	regex_t pregit g;
+	regex_t preg;
 	cmd_t *cmd = cl->cmd->head->payload;
 	char pattern[] = R"(?:\/lvlup \d+$)";
 	char usage[] = "LVLUP: [id]";
