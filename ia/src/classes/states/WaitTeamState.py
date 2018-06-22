3ecba126da9b0046173660cd94023c5565e895c5
Arzad <cedric.thomas@epitech.eu>
2018-06-22 19:46:37 +0200

    [~] fix dup conditions

--- ia/src/classes/states/WaitTeamState.py
+++ ia/src/classes/states/WaitTeamState.py
@@ -1,7 +1,7 @@
 # coding = utf-8
 import enum
 
-from src.misc import dup_me
+from src.misc import dup_me, my_print
 from src.classes.com.SafeController import safe_controller
 from src.classes.ia_res.TrackableTransactions import ForkTransaction, LookTransaction, ConnectNbrTransaction
 from src.classes.states.StateMachine import AAIState, statemachine
@@ -12,13 +12,13 @@ class WaitTeamState(AAIState):
     def __init__(self):
         super().__init__("Wait")
         self._status_stack = [
-            ForkTransaction(self.fork),
-            ConnectNbrTransaction(self.connect_nbr),
-            LookTransaction(self.wait_msg),
+            lambda: ForkTransaction(self.fork),
+            lambda: ConnectNbrTransaction(self.connect_nbr),
+            lambda: LookTransaction(self.wait_msg),
         ]
 
     def connect_nbr(self, *args):
-        if len(args) != 0:
+        if len(args) != 0 and args[0] > 0:
             dup_me()
             self._status_stack.pop(0)
         self.template()
@@ -35,7 +35,7 @@ class WaitTeamState(AAIState):
         if len(self._status_stack) == 0:
             statemachine.closure = lambda ok=None: statemachine.pop()
         else:
-            safe_controller.execute(self._status_stack[0])
+            safe_controller.execute(self._status_stack[0]())
 
     def on_push(self, cli):
         super().on_push(cli)
