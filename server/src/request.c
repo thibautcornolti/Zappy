c1cbf1f8dd3dd19a49fbc9f3c83fbd8d0864c2ca
Hirevo <nipolom@live.fr>
2018-06-24 17:16:52 +0200

    Fixed leaks

--- server/src/request.c
+++ server/src/request.c
@@ -46,6 +46,7 @@ bool handle_request(control_t *control)
 
 	control->clients = llist_filter(control->clients,
 		(bool (*)(void *, void *, size_t))(handle_client), control);
+	llist_destroy(tmp);
 	for (size_t i = 0; i < tmp->length; ++i) {
 		cl = llist_at(tmp, i);
 		if (!llist_find(control->clients, find_evicted, cl))
